10b807111b02da27689d6536841fde2b8567f896
bigwave-cho <hooyaa93@gmail.com>
2023-08-29 21:51:05 +0900

    global key 목적2: 고유 위젯임을 증명

--- lib/features/main_navigation/main_navigation.dart
+++ lib/features/main_navigation/main_navigation.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok/constants/sizes.dart';
+import 'package:tiktok/features/main_navigation/stf_screen.dart';
+import 'package:tiktok/features/main_navigation/widgets/nav_tab.dart';
 
 class MainNavigation extends StatefulWidget {
   const MainNavigation({super.key});
@@ -13,47 +16,82 @@ class _MainNavigationState extends State<MainNavigation> {
   int _selectedIndex = 0;
 
   final screens = [
-    Center(
-      child: Text('Home'),
+    // ** 글로벌키 없이 같은 스크린을 두면
+    //플러터는 다른 위젯이라고 인지하지 못하고 위젯을 그대로 사용하게 됨.
+    // 따라서 글로벌키를 이용해서 다른 위젯임을 플러터에게 알려줄 수 있음.
+    StfScreen(
+      key: GlobalKey(),
     ),
+    // Center(
+    //   child: Text('Home'),
+    // ),
+    StfScreen(
+      key: GlobalKey(),
+    ),
+
     Center(
       child: Text('Search'),
     ),
     Center(
-      child: Text('Search'),
+      child: Text('INbox'),
     ),
     Center(
-      child: Text('Search'),
-    ),
-    Center(
-      child: Text('Search'),
+      child: Text('Profile'),
     ),
   ];
+
+  void _onTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // * 쿠퍼티노 사용하려면 최상위 위젯을 cupertino app으로 바꿔줘야 함.
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.house,
-            ),
-            label: 'Home',
+    return Scaffold(
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.size12),
+          child: Row(
+            children: [
+              NavTab(
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                icon: FontAwesomeIcons.house,
+                selectedIcon: FontAwesomeIcons.house,
+                onTap: () => _onTap(0),
+              ),
+              NavTab(
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                icon: FontAwesomeIcons.compass,
+                selectedIcon: FontAwesomeIcons.solidCompass,
+                onTap: () => _onTap(1),
+              ),
+              NavTab(
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                icon: FontAwesomeIcons.message,
+                selectedIcon: FontAwesomeIcons.solidMessage,
+                onTap: () => _onTap(3),
+              ),
+              NavTab(
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                icon: FontAwesomeIcons.user,
+                selectedIcon: FontAwesomeIcons.solidUser,
+                onTap: () => _onTap(4),
+              ),
+            ],
           ),
-          BottomNavigationBarItem(
-            icon: Icon(
-              CupertinoIcons.search,
-            ),
-            label: 'Search',
-          ),
-        ],
+        ),
       ),
-      tabBuilder: (context, index) => screens[_selectedIndex],
     );
   }
 }
-
 
 /*
 
